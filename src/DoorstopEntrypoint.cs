b5c8b6b811f2265a75deb9c3edbd6186a7b3ce18
peekagrub <25369813+peekagrub@users.noreply.github.com>
2026-02-12 12:21:26 +0100

    Working doorstop mod
    Simple working doorstop mod that patches the Assembly-CSharp.dll to have
    an GameManager::OnGUI method.
    Because of using Mono.Cecil for this I have to direct the Mono runtime
    to the patched DLL which is done through the files in doorstop_config.

--- src/DoorstopEntrypoint.cs
+++ src/DoorstopEntrypoint.cs
@@ -1,11 +1,231 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using SilksongDoorstop.Patches;
+using UnityEngine;
 
 namespace Doorstop;
 
-public class Entrypoint
+public static class Entrypoint
 {
-	public static void Start()
+    public static void Start()
+    {
+
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+	string dataDir = Directory.GetDirectories(baseDir, "*_Data").FirstOrDefault();
+        string managedDir = Path.Combine(baseDir, dataDir, "Managed");
+        string asmPath = Path.Combine(managedDir, "Assembly-CSharp.dll");
+
+	string entrypointPath = typeof(Entrypoint).Assembly.Location;
+   	string modDir = Path.GetDirectoryName(entrypointPath);
+
+	var resolver = new DefaultAssemblyResolver();
+	resolver.AddSearchDirectory(managedDir);
+	resolver.AddSearchDirectory(entrypointPath);
+	resolver.AddSearchDirectory(typeof(object).Assembly.Location);
+
+	ReaderParameters readerParams = new ReaderParameters{
+	    AssemblyResolver = resolver,
+	    ReadWrite = false
+	};
+
+        AssemblyDefinition silksongAsm = AssemblyDefinition.ReadAssembly(asmPath, new ReaderParameters
+        {
+	    AssemblyResolver = resolver,
+            ReadWrite = true,
+	    InMemory = false
+        });
+
+	AssemblyDefinition doorstopAsm = AssemblyDefinition.ReadAssembly(entrypointPath, readerParams);
+
+	ModuleDefinition silksongModule = silksongAsm.MainModule;
+	TypeDefinition gamemanagerType = silksongModule.GetType("GameManager");
+
+	ModuleDefinition doorstopModule = doorstopAsm.MainModule;
+	TypeDefinition gamemanagerPatchType = doorstopModule.GetType("SilksongDoorstop.Patches.GameManagerPatch");
+
+	MethodDefinition patchMethod = gamemanagerPatchType.Methods.First(method => method.Name == "OnGUI");
+
+	MethodDefinition patchedMethod = new MethodDefinition("OnGUI", MethodAttributes.Private | MethodAttributes.HideBySig, silksongModule.ImportReference(typeof(void)));
+
+	patchedMethod.Body.InitLocals = patchMethod.Body.InitLocals;
+
+	foreach (VariableDefinition varDef in patchMethod.Body.Variables)
 	{
-		File.WriteAllText("doorstop_hello.log", "Hello from Unity");
+	    string varAssembly = String.Empty;
+	    string[] varTypeDef = varDef.VariableType.FullName.Split('.');
+	    for (int i = 0; i < varTypeDef.Length - 1; i++)
+	    {
+	        varAssembly += varTypeDef[i] + ".";
+	    }
+	    varAssembly = varAssembly.Trim('.');
+	    Type type = Type.GetType($"{varDef.VariableType.FullName}, {varAssembly}");
+
+	    if (type == null)
+	    {
+	        type = Type.GetType(varDef.VariableType.FullName);
+	        if (type == null)
+	        {
+	    	    Console.WriteLine($"Failed getting var type for: {varDef.VariableType.FullName}");
+	        }
+	    }
+
+	    TypeReference typeRef = silksongAsm.MainModule.ImportReference(type);
+
+	    patchedMethod.Body.Variables.Add(new VariableDefinition(typeRef));
 	}
+
+	ILProcessor il = patchedMethod.Body.GetILProcessor();
+
+	foreach (Instruction inst in patchMethod.Body.Instructions)
+	{
+	    if (inst.OpCode == OpCodes.Newobj)
+	    {
+	        MethodReference operand = (MethodReference)inst.Operand;
+	        string[] methodDef = operand.FullName.Split(' ', ':').Where(x => !String.IsNullOrEmpty(x)).ToArray();
+	        string assembly = String.Empty;
+	        string[] typeDef = methodDef[1].Split('.');
+	        string[] subs = methodDef[2].TrimEnd(')').Split('(', ',');
+	        subs = subs.Skip(1).ToArray().Where(x => !String.IsNullOrEmpty(x)).ToArray();
+	        Type[] args = subs.Select(arg => 
+	            {
+	            	string argAssembly = String.Empty;
+	        	string[] argTypeDef = arg.Split('.');
+	        	for (int i = 0; i < argTypeDef.Length - 1; i++)
+	        	{
+	        	    argAssembly += argTypeDef[i] + ".";
+	        	}
+	        	argAssembly = argAssembly.Trim('.');
+	        	Type type = Type.GetType($"{arg}, {argAssembly}");
+
+	        	if (type == null)
+	        	{
+	        	    type = Type.GetType(arg);
+	        	    if (type == null)
+	        	    {
+	        		Console.WriteLine($"Failed getting type for: {operand.FullName}");
+	        		Console.WriteLine($"Failed getting arg type for: {arg}");
+	        	    }
+	        	}
+	        	return type;
+	            }).ToArray();
+	        if (args == null)
+	        {
+	            Console.WriteLine($"Failed getting type for: {operand.FullName}");
+	        }
+
+	        for (int i = 0; i < typeDef.Length - 1; i++)
+	        {
+	            assembly += typeDef[i] + ".";
+	        }
+	        assembly = assembly.Trim('.');
+	        if (String.IsNullOrEmpty(assembly))
+	        {
+		     TypeDefinition classType = silksongModule.GetType(methodDef[1]);
+		     MethodDefinition constructor = classType.Methods.First(method => 
+			method.IsConstructor &&
+			method.Parameters.Count == args.Length &&
+			method.Parameters.Select(param => param.ParameterType.FullName).SequenceEqual(
+			    args.Select(arg => arg.FullName)
+			)
+		     );
+		     inst.Operand = constructor;
+	        } else {
+	            Type classType = Type.GetType($"{methodDef[1]}, {assembly}");
+	            if (classType == null)
+	            {
+	                classType = Type.GetType(methodDef[1]);
+	                if (classType == null)
+	                {
+	                    Console.WriteLine($"Failed getting type for: {operand.FullName}");
+	                }
+	            }
+	            var mi = classType.GetConstructor(args);
+	            inst.Operand = silksongAsm.MainModule.ImportReference(mi);
+		}
+
+		il.Append(inst);
+	    }
+	    else if (inst.OpCode == OpCodes.Call || inst.OpCode == OpCodes.Callvirt)
+	    {
+		MethodReference operand = (MethodReference)inst.Operand;
+		string[] methodDef = operand.FullName.Split(' ', ':').Where(x => !String.IsNullOrEmpty(x)).ToArray();
+
+		string[] subs = methodDef[2].TrimEnd(')').Split('(', ',');
+		string functionName = subs[0];
+		subs = subs.Skip(1).ToArray().Where(x => !String.IsNullOrEmpty(x)).ToArray();
+		Type[] args = subs.Select(arg => 
+		    {
+		    	string argAssembly = String.Empty;
+			string[] argTypeDef = arg.Split('.');
+			for (int i = 0; i < argTypeDef.Length - 1; i++)
+			{
+			    argAssembly += argTypeDef[i] + ".";
+			}
+			argAssembly = argAssembly.Trim('.');
+			Type type = Type.GetType($"{arg}, {argAssembly}");
+
+			if (type == null)
+			{
+			    type = Type.GetType(arg);
+			    if (type == null)
+			    {
+				Console.WriteLine($"Failed getting type for: {operand.FullName}");
+				Console.WriteLine($"Failed getting arg type for: {arg}");
+			    }
+			}
+			return type;
+		    }).ToArray();
+		if (args == null)
+		{
+		    Console.WriteLine($"Failed getting type for: {operand.FullName}");
+		}
+
+		string assembly = String.Empty;
+		string[] typeDef = methodDef[1].Split('.');
+		for (int i = 0; i < typeDef.Length - 1; i++)
+		{
+		    assembly += typeDef[i] + ".";
+		}
+		assembly = assembly.Trim('.');
+		if (String.IsNullOrEmpty(assembly))
+		{
+		     TypeDefinition classType = silksongModule.GetType(methodDef[1]);
+		     MethodDefinition constructor = classType.Methods.First(method => 
+		        method.Name == functionName &&
+		        method.Parameters.Count == args.Length &&
+		        method.Parameters.Select(param => param.ParameterType.FullName).SequenceEqual(
+		                args.Select(arg => arg.FullName)
+	                )
+		     );
+		     inst.Operand = constructor;
+		} else 
+		{
+	            Type classType = Type.GetType($"{methodDef[1]}, {assembly}");
+	            if (classType == null)
+	            {
+	                classType = Type.GetType(methodDef[1]);
+	                if (classType == null)
+	                {
+	                    Console.WriteLine($"Failed getting type for: {operand.FullName}");
+	                }
+	            }
+	            var mi = classType.GetMethod(functionName, args);
+	            inst.Operand = silksongAsm.MainModule.ImportReference(mi);
+		}
+
+		il.Append(inst);
+	    } else 
+	    {
+	        il.Append(inst);
+	    }
+	}
+
+	gamemanagerType.Methods.Add(patchedMethod);
+
+	silksongAsm.Write(Path.Combine(modDir, "Assembly-CSharp.dll"));
+	silksongAsm.Dispose();
+    }
 }
---
+++ src/GameManager.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+namespace SilksongDoorstop.Patches;
+
+public class GameManagerPatch : global::GameManager
+{
+    private void OnGUI()
+    {
+        if (this.GetSceneNameString() == Constants.MENU_SCENE)
+        {
+            var oldBackgroundColor = GUI.backgroundColor;
+            var oldContentColor = GUI.contentColor;
+            var oldColor = GUI.color;
+            var oldMatrix = GUI.matrix;
+
+            GUI.backgroundColor = Color.white;
+            GUI.contentColor = Color.white;
+            GUI.color = Color.white;
+            GUI.matrix = Matrix4x4.TRS(
+                Vector3.zero,
+                Quaternion.identity,
+                new Vector3(Screen.width / 1920f, Screen.height / 1080f, 1f)
+            );
+
+            string WarningText = "Doorstop Patches";
+
+            GUI.Label(
+                new Rect(20f, 20f, 200f, 200f),
+                WarningText,
+                new GUIStyle
+                {
+                    fontSize = 30,
+                    normal = new GUIStyleState
+                    {
+                        textColor = Color.white,
+                    }
+                }
+            );
+
+            GUI.backgroundColor = oldBackgroundColor;
+            GUI.contentColor = oldContentColor;
+            GUI.color = oldColor;
+            GUI.matrix = oldMatrix;
+        }
+    }
+}
