bcafc2fcabe0b674c519e0624c1e1fb9ec824187
peekagrub <25369813+peekagrub@users.noreply.github.com>
2026-02-14 18:14:28 +0100

    Cleanup code
    Didn't need any of the complicated parsing stuff, I could just directly
    use the refences I already got for the importing.

--- src/DoorstopEntrypoint.cs
+++ src/DoorstopEntrypoint.cs
@@ -31,12 +31,7 @@ public static class Entrypoint
             ReadWrite = false
         };
 
-        AssemblyDefinition silksongAsm = AssemblyDefinition.ReadAssembly(asmPath, new ReaderParameters
-        {
-            AssemblyResolver = resolver,
-            ReadWrite = true,
-            InMemory = false
-        });
+        AssemblyDefinition silksongAsm = AssemblyDefinition.ReadAssembly(asmPath, readerParams);
 
         AssemblyDefinition doorstopAsm = AssemblyDefinition.ReadAssembly(entrypointPath, readerParams);
 
@@ -54,180 +49,27 @@ public static class Entrypoint
 
         foreach (VariableDefinition varDef in patchMethod.Body.Variables)
         {
-            string varAssembly = String.Empty;
-            string[] varTypeDef = varDef.VariableType.FullName.Split('.');
-            for (int i = 0; i < varTypeDef.Length - 1; i++)
-            {
-                varAssembly += varTypeDef[i] + ".";
-            }
-            varAssembly = varAssembly.Trim('.');
-            Type type = Type.GetType($"{varDef.VariableType.FullName}, {varAssembly}");
-
-            if (type == null)
-            {
-                type = Type.GetType(varDef.VariableType.FullName);
-                if (type == null)
-                {
-                    Console.WriteLine($"Failed getting var type for: {varDef.VariableType.FullName}");
-                }
-            }
-
-            TypeReference typeRef = silksongAsm.MainModule.ImportReference(type);
-
-            patchedMethod.Body.Variables.Add(new VariableDefinition(typeRef));
+            varDef.VariableType = silksongModule.ImportReference(varDef.VariableType);
+            patchedMethod.Body.Variables.Add(varDef);
         }
 
         ILProcessor il = patchedMethod.Body.GetILProcessor();
 
         foreach (Instruction inst in patchMethod.Body.Instructions)
         {
-            if (inst.OpCode == OpCodes.Newobj)
+            if (inst.OpCode == OpCodes.Call || inst.OpCode == OpCodes.Callvirt || inst.OpCode == OpCodes.Newobj)
             {
-                MethodReference operand = (MethodReference)inst.Operand;
-                string[] methodDef = operand.FullName.Split(' ', ':').Where(x => !String.IsNullOrEmpty(x)).ToArray();
-                string assembly = String.Empty;
-                string[] typeDef = methodDef[1].Split('.');
-                string[] subs = methodDef[2].TrimEnd(')').Split('(', ',');
-                subs = subs.Skip(1).ToArray().Where(x => !String.IsNullOrEmpty(x)).ToArray();
-                Type[] args = subs.Select(arg =>
-                    {
-                        string argAssembly = String.Empty;
-                        string[] argTypeDef = arg.Split('.');
-                        for (int i = 0; i < argTypeDef.Length - 1; i++)
-                        {
-                            argAssembly += argTypeDef[i] + ".";
-                        }
-                        argAssembly = argAssembly.Trim('.');
-                        Type type = Type.GetType($"{arg}, {argAssembly}");
-
-                        if (type == null)
-                        {
-                            type = Type.GetType(arg);
-                            if (type == null)
-                            {
-                                Console.WriteLine($"Failed getting type for: {operand.FullName}");
-                                Console.WriteLine($"Failed getting arg type for: {arg}");
-                            }
-                        }
-                        return type;
-                    }).ToArray();
-                if (args == null)
-                {
-                    Console.WriteLine($"Failed getting type for: {operand.FullName}");
-                }
-
-                for (int i = 0; i < typeDef.Length - 1; i++)
-                {
-                    assembly += typeDef[i] + ".";
-                }
-                assembly = assembly.Trim('.');
-                if (String.IsNullOrEmpty(assembly))
-                {
-                    TypeDefinition classType = silksongModule.GetType(methodDef[1]);
-                    MethodDefinition constructor = classType.Methods.First(method =>
-                        method.IsConstructor &&
-                        method.Parameters.Count == args.Length &&
-                        method.Parameters.Select(param => param.ParameterType.FullName).SequenceEqual(
-                            args.Select(arg => arg.FullName)
-                        ));
-                    inst.Operand = constructor;
-                }
-                else
-                {
-                    Type classType = Type.GetType($"{methodDef[1]}, {assembly}");
-                    if (classType == null)
-                    {
-                        classType = Type.GetType(methodDef[1]);
-                        if (classType == null)
-                        {
-                            Console.WriteLine($"Failed getting type for: {operand.FullName}");
-                        }
-                    }
-                    var mi = classType.GetConstructor(args);
-                    inst.Operand = silksongAsm.MainModule.ImportReference(mi);
-                }
-
-                il.Append(inst);
+                inst.Operand = silksongModule.ImportReference((MethodReference)inst.Operand);
             }
-            else if (inst.OpCode == OpCodes.Call || inst.OpCode == OpCodes.Callvirt)
-            {
-                MethodReference operand = (MethodReference)inst.Operand;
-                string[] methodDef = operand.FullName.Split(' ', ':').Where(x => !String.IsNullOrEmpty(x)).ToArray();
-
-                string[] subs = methodDef[2].TrimEnd(')').Split('(', ',');
-                string functionName = subs[0];
-                subs = subs.Skip(1).ToArray().Where(x => !String.IsNullOrEmpty(x)).ToArray();
-                Type[] args = subs.Select(arg =>
-                    {
-                        string argAssembly = String.Empty;
-                        string[] argTypeDef = arg.Split('.');
-                        for (int i = 0; i < argTypeDef.Length - 1; i++)
-                        {
-                            argAssembly += argTypeDef[i] + ".";
-                        }
-                        argAssembly = argAssembly.Trim('.');
-                        Type type = Type.GetType($"{arg}, {argAssembly}");
-
-                        if (type == null)
-                        {
-                            type = Type.GetType(arg);
-                            if (type == null)
-                            {
-                                Console.WriteLine($"Failed getting type for: {operand.FullName}");
-                                Console.WriteLine($"Failed getting arg type for: {arg}");
-                            }
-                        }
-                        return type;
-                    }).ToArray();
-                if (args == null)
-                {
-                    Console.WriteLine($"Failed getting type for: {operand.FullName}");
-                }
-
-                string assembly = String.Empty;
-                string[] typeDef = methodDef[1].Split('.');
-                for (int i = 0; i < typeDef.Length - 1; i++)
-                {
-                    assembly += typeDef[i] + ".";
-                }
-                assembly = assembly.Trim('.');
-                if (String.IsNullOrEmpty(assembly))
-                {
-                    TypeDefinition classType = silksongModule.GetType(methodDef[1]);
-                    MethodDefinition constructor = classType.Methods.First(method =>
-                        method.Name == functionName &&
-                        method.Parameters.Count == args.Length &&
-                        method.Parameters.Select(param => param.ParameterType.FullName).SequenceEqual(
-                            args.Select(arg => arg.FullName)
-                        ));
-                    inst.Operand = constructor;
-                }
-                else
-                {
-                    Type classType = Type.GetType($"{methodDef[1]}, {assembly}");
-                    if (classType == null)
-                    {
-                        classType = Type.GetType(methodDef[1]);
-                        if (classType == null)
-                        {
-                            Console.WriteLine($"Failed getting type for: {operand.FullName}");
-                        }
-                    }
-                    var mi = classType.GetMethod(functionName, args);
-                    inst.Operand = silksongAsm.MainModule.ImportReference(mi);
-                }
-
-                il.Append(inst);
-            }
-            else
-            {
-                il.Append(inst);
-            }
+            il.Append(inst);
         }
 
         gamemanagerType.Methods.Add(patchedMethod);
 
         silksongAsm.Write(Path.Combine(modDir, "Assembly-CSharp.dll"));
+
+        // Cleanup resources
         silksongAsm.Dispose();
+        doorstopAsm.Dispose();
     }
 }
